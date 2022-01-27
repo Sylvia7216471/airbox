39c81ad9c2499c04b3d130087e252da5ab2cd3d7
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-01-27 14:58:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,66 +31,33 @@ line_bot_api.push_message('Uc2c240ca992c30a49afa0a29288ee53d', TextSendMessage(t
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+ 
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+ 
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-     message = text=event.message.text
-     if re.match('告訴我秘密',message):
-         carousel_template_message = TemplateSendMessage(
-             alt_text='免費教學影片',
-             template=CarouselTemplate(
-                 columns=[
-                     CarouselColumn(
-                         thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
-                         title='Python基礎教學',
-                         text='萬丈高樓平地起',
-                         actions=[
-                             MessageAction(
-                                 label='教學內容',
-                                 text='拆解步驟詳細介紹安裝並使用Anaconda、Python、Spyder、VScode…'
-                             ),
-                             URIAction(
-                                 label='馬上查看',
-                                 uri='https://marketingliveincode.com/?page_id=270'
-                             )
-                         ]
-                     ),
-                     CarouselColumn(
-                         thumbnail_image_url='https://i.imgur.com/W7nI6fg.jpg',
-                         title='Line Bot聊天機器人',
-                         text='台灣最廣泛使用的通訊軟體',
-                         actions=[
-                             MessageAction(
-                                 label='教學內容',
-                                 text='Line Bot申請與串接'
-                             ),
-                             URIAction(
-                                 label='馬上查看',
-                                 uri='https://marketingliveincode.com/?page_id=2532'
-                             )
-                         ]
-                     ),
-                     CarouselColumn(
-                         thumbnail_image_url='https://i.imgur.com/l7rzfIK.jpg',
-                         title='Telegram Bot聊天機器人',
-                         text='唯有真正的方便，能帶來意想不到的價值',
-                         actions=[
-                             MessageAction(
-                                 label='教學內容',
-                                 text='Telegrame申請與串接'
-                             ),
-                             URIAction(
-                                 label='馬上查看',
-                                 uri='https://marketingliveincode.com/?page_id=2648'
-                             )
-                         ]
-                     )
-                 ]
-             )
-         )
-         line_bot_api.reply_message(event.reply_token, carousel_template_message)
-     else:
-         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
- # 主程式
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
+
+#主程式
 import os
-if name == "main":
-   port = int(os.environ.get('PORT', 5000))
-   app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
