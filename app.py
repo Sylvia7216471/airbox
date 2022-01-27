2f7471d07c4be9724a6f7828c3176bd09354b6ab
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-01-27 15:07:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,65 +32,65 @@ line_bot_api.push_message('Uc2c240ca992c30a49afa0a29288ee53d', TextSendMessage(t
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('我想選擇裝置',message):
-        carousel_template_message = TemplateSendMessage(
-             alt_text='多樣版組合按鈕',
+     message = text=event.message.text
+     if re.match('告訴我秘密',message):
+         carousel_template_message = TemplateSendMessage(
+             alt_text='免費教學影片',
              template=CarouselTemplate(
                  columns=[
                      CarouselColumn(
                          thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
-                         title='裝置1',
-                         text='請選擇需要的功能',
+                         title='Python基礎教學',
+                         text='萬丈高樓平地起',
                          actions=[
                              MessageAction(
-                                 label='環境資料',
-                                 text=''
+                                 label='教學內容',
+                                 text='拆解步驟詳細介紹安裝並使用Anaconda、Python、Spyder、VScode…'
                              ),
-                             MessageAction(
-                                 label='裝置位置',
-                                 text=''
+                             URIAction(
+                                 label='馬上查看',
+                                 uri='https://marketingliveincode.com/?page_id=270'
                              )
                          ]
                      ),
-                    CarouselColumn(
-                         thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
-                         title='裝置2',
-                         text='請選擇需要的功能',
+                     CarouselColumn(
+                         thumbnail_image_url='https://i.imgur.com/W7nI6fg.jpg',
+                         title='Line Bot聊天機器人',
+                         text='台灣最廣泛使用的通訊軟體',
                          actions=[
                              MessageAction(
-                                 label='環境資料',
-                                 text=''
+                                 label='教學內容',
+                                 text='Line Bot申請與串接'
                              ),
-                             MessageAction(
-                                 label='裝置位置',
-                                 text=''
+                             URIAction(
+                                 label='馬上查看',
+                                 uri='https://marketingliveincode.com/?page_id=2532'
                              )
                          ]
                      ),
-                    CarouselColumn(
-                         thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
-                         title='裝置3',
-                         text='請選擇需要的功能',
+                     CarouselColumn(
+                         thumbnail_image_url='https://i.imgur.com/l7rzfIK.jpg',
+                         title='Telegram Bot聊天機器人',
+                         text='唯有真正的方便，能帶來意想不到的價值',
                          actions=[
                              MessageAction(
-                                 label='環境資料',
-                                 text=''
+                                 label='教學內容',
+                                 text='Telegrame申請與串接'
                              ),
-                             MessageAction(
-                                 label='裝置位置',
-                                 text=''
+                             URIAction(
+                                 label='馬上查看',
+                                 uri='https://marketingliveincode.com/?page_id=2648'
                              )
                          ]
                      )
                  ]
              )
          )
-        line_bot_api.reply_message(event.reply_token, carousel_template_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+         line_bot_api.reply_message(event.reply_token, carousel_template_message)
+     else:
+         line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
  # 主程式
-import os
-if name == "main":
-   port = int(os.environ.get('PORT', 5000))
-   app.run(host='0.0.0.0', port=port)
+ import os
+ if name == "main":
+     port = int(os.environ.get('PORT', 5000))
+     app.run(host='0.0.0.0', port=port)
