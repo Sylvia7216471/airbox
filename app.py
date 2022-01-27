c5fc05b5052434e2f3acc5659aee3b5305887544
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-01-27 16:11:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,33 +4,35 @@ Created on Wed Jun  2 21:16:35 2021
 @author: Ivan
 版權屬於「行銷搬進大程式」所有，若有疑問，可聯絡ivanyang0606@gmail.com
 Line Bot聊天機器人
-第一章 Line Bot申請與串接
-Line Bot機器人串接與測試
+第三章 互動回傳功能
+傳送圖片ImageSendMessage
 """
 #載入LineBot所需要的套件
 from flask import Flask, request, abort
+
 from linebot import (
-     LineBotApi, WebhookHandler
- )
+    LineBotApi, WebhookHandler
+)
 from linebot.exceptions import (
-     InvalidSignatureError
+    InvalidSignatureError
 )
 from linebot.models import *
 import re
- 
 app = Flask(__name__)
- # 必須放上自己的Channel Access Token
+
+# 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('0ZtGtrSOwbENt1n2Z0cehZ610JCO8aaojjiqtyOJlpt1/M85m3UpKTWiEpfmx+2vhLTgSLwnFe8DRpUQBsWjTWNUhG5O9KnlAbF7IIYWbPYKh+CLZ9E0c9H4FG0bCfGdpcbajZ42KRQgmexjrNbQzQdB04t89/1O/w1cDnyilFU=')
- # 必須放上自己的Channel Secret
+# 必須放上自己的Channel Secret
 handler = WebhookHandler('01bbcb3ed094f619ac3be5b6fe352942')
+
 line_bot_api.push_message('Uc2c240ca992c30a49afa0a29288ee53d', TextSendMessage(text='你可以開始了'))
- # 監聽所有來自 /callback 的 Post Request
+
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
- 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
@@ -43,14 +45,19 @@ def callback():
 
     return 'OK'
 
- 
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
-
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        image_message = ImageSendMessage(
+            original_content_url='https://images.builderservices.io/s/cdn/v1.0/i/m?url=https%3A%2F%2Fstorage.googleapis.com%2Fproduction-bluehost-v1-0-9%2F659%2F790659%2FAtmP8Pmy%2F9c8c1e647eb14e01898043c0c60bf03a&methods=resize%2C1000%2C5000',
+            preview_image_url='https://images.builderservices.io/s/cdn/v1.0/i/m?url=https%3A%2F%2Fstorage.googleapis.com%2Fproduction-bluehost-v1-0-9%2F659%2F790659%2FAtmP8Pmy%2Ffd2258c5ea6c43f591e8d9930d152b94&methods=resize%2C1000%2C5000'
+        )
+        line_bot_api.reply_message(event.reply_token, image_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 #主程式
 import os
 if __name__ == "__main__":
