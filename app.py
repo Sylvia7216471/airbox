c379b7101dc231df207a3785ac3cbf91c05836a6
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-01-28 13:43:59 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -136,8 +136,8 @@ def handle_message(event):
             )
         )
         line_bot_api.reply_message(event.reply_token, carousel_template_message)
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+   # else:
+      #  line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 #主程式
 import os
 if __name__ == "__main__":
