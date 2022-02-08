5dfb261b915504b7ce9c28e9ba1c1d8b6b2f09b9
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-02-08 14:32:41 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -168,9 +168,9 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, carousel_template_message)
     elif re.match('環境資料',message):
         line_bot_api.push_message(yourID, 
-                         TextSendMessage(text='更新時間:')+str(datetime_format))
-       # line_bot_api.push_message(yourID, 
-        #                  TextSendMessage(str(datetime.datetime.now())))
+                         TextSendMessage(text='更新時間:'))
+        line_bot_api.push_message(yourID, 
+                          TextSendMessage(str(datetime_format)))
         line_bot_api.push_message(yourID, 
                           TextSendMessage(text='溫度:\n濕度:\n懸浮粒子(ug/m3)\nPM1:\nPM2.5:\nPM10:\n氣體感測\nMQ3:(ug/L)\nMQ7:(ppm)\nMQ135:(ppm)'))
 #主程式
