e67eedc34289f882748c43cd72a3e61ce7dbb8d3
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-02-08 14:15:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ def handle_message(event):
       #  line_bot_api.push_message(yourID, 
        #                   TextSendMessage(datetime_str))
         line_bot_api.push_message(yourID, 
-                          TextSendMessage(text='\n溫度:\n濕度:\n懸浮粒子(ug/m3)\nPM1:\nPM2.5:\nPM10:\n氣體感測\nMQ3:(ug/L)\nMQ7:(ppm)\nMQ135:(ppm)'))
+                          TextSendMessage(text='更新時間:\n溫度:\n濕度:\n懸浮粒子(ug/m3)\nPM1:\nPM2.5:\nPM10:\n氣體感測\nMQ3:(ug/L)\nMQ7:(ppm)\nMQ135:(ppm)'))
 #主程式
 import os
 if __name__ == "__main__":
