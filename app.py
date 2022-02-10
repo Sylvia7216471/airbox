e2447caa9a181f88ce66813b4dc811fefba393eb
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-02-10 17:18:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,48 +10,6 @@ Line Bot聊天機器人
 多樣版組合按鈕CarouselTemplate
 """
 	
-import paho.mqtt.client as mqtt
-import json
-
-# 當地端程式連線伺服器得到回應時，要做的動作
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-
-    # 將訂閱主題寫在on_connet中
-    # 如果我們失去連線或重新連線時 
-    # 地端程式將會重新訂閱
-    client.subscribe("liru")
-
-# 當接收到從伺服器發送的訊息時要進行的動作
-def on_message(client, userdata, msg):
-    # 轉換編碼utf-8才看得懂中文
-    json_file_path = "/path/to/example.json"
-    payload = msg.payload.decode('utf-8')
-    print(msg.topic+" "+ msg.payload.decode('utf-8'))
-
-
-# 連線設定
-# 初始化地端程式
-client = mqtt.Client()
-
-# 設定連線的動作
-client.on_connect = on_connect
-
-# 設定接收訊息的動作
-client.on_message = on_message
-
-# 設定登入帳號密碼
-#client.username_pw_set("cubie","你的MQTT API Key")
-
-# 設定連線資訊(IP, Port, 連線時間)
-client.connect("120.119.157.238", 1800, 60)
-
-# 開始連線，執行設定的動作和處理重新連線問題
-# 也可以手動使用其他loop函式來進行連接
-client.loop_forever()
-
-
-
 
 #載入LineBot所需要的套件
 from flask import Flask, request, abort
