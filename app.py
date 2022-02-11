3619fc6d16e59d909337e745c6eb6708f72b6ce4
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-02-11 15:07:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,40 +11,30 @@ Line Bot聊天機器人
 """
 
 import paho.mqtt.client as mqtt
+import ssl
+import sys
 
-# Define event callbacks
+#user = "IAMNOTTELLINGYOU"
+#passw ="IAMNOTTELLINGYOU"
+# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+	print("Connected with result code "+str(rc))
+	client.subscribe("liru")
 
-    # 將訂閱主題寫在on_connet中
-    # 如果我們失去連線或重新連線時 
-    # 地端程式將會重新訂閱
-    client.subscribe("liru")
-
-# 當接收到從伺服器發送的訊息時要進行的動作
+# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    # 轉換編碼utf-8才看得懂中文
-    print(msg.topic+" "+ msg.payload.decode('utf-8'))
+	print('Topic:', msg.topic+"\nMessage: "+str(msg.payload))
 
-# 連線設定
-# 初始化地端程式
-client = mqtt.Client()
+client = mqtt.Client(transport="websockets")
+client.tls_set()
+client.tls_insecure_set(True) 
 
-# 設定連線的動作
+#client.username_pw_set(user,passw)
 client.on_connect = on_connect
-
-# 設定接收訊息的動作
 client.on_message = on_message
 
-# 設定登入帳號密碼
-#client.username_pw_set("try","xxxx")
-
-# 設定連線資訊(IP, Port, 連線時間)
 client.connect("120.119.157.238", 1800, 60)
-
-# 開始連線，執行設定的動作和處理重新連線問題
-# 也可以手動使用其他loop函式來進行連接
-client.loop_forever()
+#client.publish("data/deviceInfo", "Hello", qos=0, retain=False)
 
 
 #載入LineBot所需要的套件
