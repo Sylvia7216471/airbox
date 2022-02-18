ce3b34087e9907d90baf1e5e597983e2be95058e
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-02-18 18:55:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -66,9 +66,7 @@ def handle_message(event):
     new_dt = loc_dt - time_del 
     datetime_format = new_dt.strftime("%Y/%m/%d %H:%M:%S")
     loc_dt_format = loc_dt.strftime("%Y/%m/%d %H:%M:%S")
-    cursor.execute("SELECT * FROM device_5")
-    result = cursor.fetchone()
-for row in result:
+   
  #  message2="裝置1\n更新時間:\n溫度:\n濕度:\n懸浮粒子(ug/m3)\nPM1:\nPM2.5:\nPM10:\n氣體感測\nMQ3:(ug/L)\nMQ7:(ppm)\nMQ135:(ppm)"
     yourID = 'Uc2c240ca992c30a49afa0a29288ee53d'
     if re.match('我想選擇裝置',message):
@@ -182,7 +180,7 @@ for row in result:
         line_bot_api.push_message(yourID, 
                           TextSendMessage(str(datetime_format)))
         line_bot_api.push_message(yourID, 
-                          TextSendMessage(text='溫度:\n濕度:\n懸浮粒子(ug/m3)\nPM1:\nPM2.5:\nPM10:\n氣體感測\nMQ3:(ug/L)\nMQ7:(ppm)\nMQ135:(ppm)'+row))
+                          TextSendMessage(text='溫度:\n濕度:\n懸浮粒子(ug/m3)\nPM1:\nPM2.5:\nPM10:\n氣體感測\nMQ3:(ug/L)\nMQ7:(ppm)\nMQ135:(ppm)'))
     elif re.match('環境資料2',message):
         line_bot_api.push_message(yourID, 
                          TextSendMessage(text='更新時間:'))
