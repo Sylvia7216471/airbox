13d85e6b2759fbae3024df1615634ad79dc6caf7
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-02-18 15:57:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from linebot.exceptions import (
 from linebot.models import *
 import re
 app = Flask(__name__)
-
+import os
 import datetime
 import pymysql
 
@@ -34,7 +34,7 @@ line_bot_api.push_message('Uc2c240ca992c30a49afa0a29288ee53d', TextSendMessage(t
 
 #資料庫連線設定
 #可縮寫db = pymysql.connect("localhost","root","root","30days" )
-db = pymysql.connect(host='120.119.157.238', port=1801, user='LiRu', passwd='', database = "iot")
+db = pymysql.connect(host=os.environ["IP"], port=os.environ["PORT"], user='LiRu', passwd='', database = "iot")
 #建立操作游標
 cursor = db.cursor()
 
