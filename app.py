660c63fdca09099ae83685ac9cf680f125a872d6
Sylvia7216471 <97227004+Sylvia7216471@users.noreply.github.com>
2022-02-25 17:49:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,21 @@ def callback():
 #資料庫連線設定
 db = pymysql.connect(host='us-cdbr-east-05.cleardb.net', port='', user='b27ce87b980a11', passwd='167961db', database = "heroku_5ce6e6298fde0f2")
 
+cursor1 = db.cursor()
+cursor1.execute("SELECT * FROM device_1 ORDER BY time DESC LIMIT 1")
+result1 = cursor1.fetchone()
+result11=result1[1]
+result21=result1[2]
+result31=result1[3]
+result41=result1[4]
+result51=result1[5]
+result61=result1[6]
+result71=result1[7]
+result81=result1[8]
+result91=result1[9]
+result101=result1[10]
+result111=result1[11]
+
 cursor2 = db.cursor()
 cursor2.execute("SELECT * FROM device_5 ORDER BY time DESC LIMIT 1")
 result2 = cursor2.fetchone()
@@ -68,6 +83,36 @@ result82=result2[8]
 result92=result2[9]
 result102=result2[10]
 result112=result2[11]
+
+cursor3 = db.cursor()
+cursor3.execute("SELECT * FROM device_3 ORDER BY time DESC LIMIT 1")
+result3 = cursor3.fetchone()
+result13=result3[1]
+result23=result3[2]
+result33=result3[3]
+result43=result3[4]
+result53=result3[5]
+result63=result3[6]
+result73=result3[7]
+result83=result3[8]
+result93=result3[9]
+result103=result3[10]
+result113=result3[11]
+
+cursor4 = db.cursor()
+cursor4.execute("SELECT * FROM device_4 ORDER BY time DESC LIMIT 1")
+result4 = cursor4.fetchone()
+result14=result4[1]
+result24=result4[2]
+result34=result4[3]
+result44=result4[4]
+result54=result4[5]
+result64=result4[6]
+result74=result4[7]
+result84=result4[8]
+result94=result4[9]
+result104=result4[10]
+result114=result4[11]
 
 cursor5 = db.cursor()
 cursor5.execute("SELECT * FROM device_5 ORDER BY time DESC LIMIT 1")
@@ -207,7 +252,7 @@ def handle_message(event):
                          TextSendMessage(text='更新時間:'+str(datetime_format)+'\n'+'測量時間:'+str(result11)+'\n'+'溫度: '+str(result21)+'˚C'+'\n'+'濕度: '+str(result31)+'RH'+'\n'+'PM1: '+str(result41)+'μg/m3'+'\n'+'PM25: '+str(result51)+'μg/m3'+'\n'+'PM10: '+str(result61)+'μg/m3'+'\n'+'MQ3: '+str(result71)+'μg/L'+'\n'+'MQ10: '+str(result81)+'μg/L'+'\n'+'MQ135: '+str(result91)+'μg/L')) 
     elif re.match('環境資料2',message):
         line_bot_api.push_message(yourID, 
-                         TextSendMessage(text='更新時間:'+str(datetime_format)+'\n'+'測量時間:'+str(result12)+'\n'+'溫度: '+str(result23)+'˚C'+'\n'+'濕度: '+str(result32)+'RH'+'\n'+'PM1: '+str(result42)+'μg/m3'+'\n'+'PM25: '+str(result52)+'μg/m3'+'\n'+'PM10: '+str(result62)+'μg/m3'+'\n'+'MQ3: '+str(result72)+'μg/L'+'\n'+'MQ10: '+str(result82)+'μg/L'+'\n'+'MQ135: '+str(result92)+'μg/L'))
+                         TextSendMessage(text='更新時間:'+str(datetime_format)+'\n'+'測量時間:'+str(result12)+'\n'+'溫度: '+str(result22)+'˚C'+'\n'+'濕度: '+str(result32)+'RH'+'\n'+'PM1: '+str(result42)+'μg/m3'+'\n'+'PM25: '+str(result52)+'μg/m3'+'\n'+'PM10: '+str(result62)+'μg/m3'+'\n'+'MQ3: '+str(result72)+'μg/L'+'\n'+'MQ10: '+str(result82)+'μg/L'+'\n'+'MQ135: '+str(result92)+'μg/L'))
     elif re.match('環境資料3',message):
           line_bot_api.push_message(yourID, 
                          TextSendMessage(text='更新時間:'+str(datetime_format)+'\n'+'測量時間:'+str(result13)+'\n'+'溫度: '+str(result23)+'˚C'+'\n'+'濕度: '+str(result33)+'RH'+'\n'+'PM1: '+str(result43)+'μg/m3'+'\n'+'PM25: '+str(result53)+'μg/m3'+'\n'+'PM10: '+str(result63)+'μg/m3'+'\n'+'MQ3: '+str(result73)+'μg/L'+'\n'+'MQ10: '+str(result83)+'μg/L'+'\n'+'MQ135: '+str(result93)+'μg/L'))
